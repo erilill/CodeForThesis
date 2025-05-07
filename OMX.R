7567a54272c9f2f5d3f9d0bf4ad6ee96c6c23b47
Erik Lillrank <erik.lillrank@gmail.com>
2025-05-07 15:12:33 +0200

    Updated how number of factors is computed to ensure that it is updated yearly

--- OMX.R
+++ OMX.R
@@ -159,9 +159,10 @@ risk_free <- risk_free[-zero_rows]
 ###############################
 # p=50
 # Select 50 random stocks
-random50 <- sample(1:347, 50)
-returns50 <- as.matrix(returns[, c(random50)])
-saveRDS(returns50, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/returns_used_in_analysis_50.rds")
+#random50 <- sample(1:347, 50)
+#returns50 <- as.matrix(returns[, c(random50)])
+#saveRDS(returns50, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/returns_used_in_analysis_50.rds")
+returns50 <- readRDS("C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/returns_used_in_analysis_50.rds")
 
 start.time <- Sys.time()
 rolling_window_results_month_2021_2024 <- mega_rol_pred_parallel(returns50, 252, 21, rf=risk_free, max_factors = 10)
@@ -169,7 +170,7 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_month_2021_2024$stats
-saveRDS(rolling_window_results_month_2021_2024, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_month_2021_2024.rds")
+saveRDS(rolling_window_results_month_2021_2024, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_month_2021_2024_50.rds")
 
 start.time <- Sys.time()
 rolling_window_results_week_2021_2024 <- mega_rol_pred_parallel(returns50, 252, 5, rf=risk_free, max_factors = 10)
@@ -177,16 +178,17 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_week_2021_2024$stats
-saveRDS(rolling_window_results_week_2021_2024, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_week_2021_2024.rds")
+saveRDS(rolling_window_results_week_2021_2024, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_week_2021_2024_50.rds")
 
 
 ################################################################################
 # p=150
 
 # Select 150 random stocks
-random150 <- sample(1:347, 150)
-returns150 <- as.matrix(returns[, c(random150)])
-saveRDS(returns150, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/returns_used_in_analysis_150.rds")
+#random150 <- sample(1:347, 150)
+#returns150 <- as.matrix(returns[, c(random150)])
+#saveRDS(returns150, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/returns_used_in_analysis_150.rds")
+returns150 <- readRDS("C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/returns_used_in_analysis_150.rds")
 
 start.time <- Sys.time()
 rolling_window_results_month_2021_2024_150 <- mega_rol_pred_parallel(returns150, 252, 21, rf=risk_free, max_factors = 10)
@@ -194,7 +196,7 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_month_2021_2024_150$stats
-saveRDS(rolling_window_results_month_2021_2024_150, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_month_2021_2024_150.rds")
+saveRDS(rolling_window_results_month_2021_2024_150, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_month_2021_2024_150.rds")
 
 
 start.time <- Sys.time()
@@ -203,16 +205,17 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_week_2021_2024_150$stats
-saveRDS(rolling_window_results_week_2021_2024_150, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_month_2021_2024_150.rds")
+saveRDS(rolling_window_results_week_2021_2024_150, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_week_2021_2024_150.rds")
 
 
 ################################################################################
 # p=250
 
 # Select 250 random stocks
-random250 <- sample(1:347, 250)
-returns250 <- as.matrix(returns[, c(random250)])
-saveRDS(returns250, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/returns_used_in_analysis_250.rds")
+#random250 <- sample(1:347, 250)
+#returns250 <- as.matrix(returns[, c(random250)])
+#saveRDS(returns250, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/returns_used_in_analysis_250.rds")
+returns250 <- readRDS("C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/returns_used_in_analysis_250.rds")
 
 start.time <- Sys.time()
 rolling_window_results_month_2021_2024_250 <- mega_rol_pred_parallel(returns250, 252, 21, rf=risk_free, max_factors = 10)
@@ -220,7 +223,7 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_month_2021_2024_250$stats
-saveRDS(rolling_window_results_week_2021_2024_250, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_week_2021_2024_250.rds")
+saveRDS(rolling_window_results_week_2021_2024_250, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_month_2021_2024_250.rds")
 
 
 start.time <- Sys.time()
@@ -229,7 +232,7 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_week_2021_2024_250$stats
-saveRDS(rolling_window_results_week_2021_2024_250, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_week_2021_2024_250.rds")
+saveRDS(rolling_window_results_week_2021_2024_250, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_week_2021_2024_250.rds")
 
 
 ##############################
@@ -246,7 +249,7 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_daily_2021_2024_50$stats
-saveRDS(rolling_window_results_daily_2021_2024_50, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_daily_2021_2024_50.rds")
+saveRDS(rolling_window_results_daily_2021_2024_50, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_daily_2021_2024_50.rds")
 
 
 start.time <- Sys.time()
@@ -255,7 +258,7 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_daily_2021_2024_150$stats
-saveRDS(rolling_window_results_daily_2021_2024_150, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_daily_2021_2024_150.rds")
+saveRDS(rolling_window_results_daily_2021_2024_150, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_daily_2021_2024_150.rds")
 
 
 start.time <- Sys.time()
@@ -264,7 +267,7 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 rolling_window_results_daily_2021_2024_250$stats
-saveRDS(rolling_window_results_daily_2021_2024_250, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/Data/rolling_window_results_daily_2021_2024_250.rds")
+saveRDS(rolling_window_results_daily_2021_2024_250, "C:/Users/erikl_xzy542i/Documents/Master_local/Thesis/updated_results/rolling_window_results_daily_2021_2024_250.rds")
 
 
 
